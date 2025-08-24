eb5da148705a3fd7ef11075508162abaa7736882
lorenqr <lorenagr223@gmail.com>
2025-08-23 19:27:58 -0500

    Creación carpetas para el fronted y backed

---
+++ back/ecocaccion-server/src/test/java/com/ecoaccion/api/EcocaccionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoaccion.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcocaccionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
